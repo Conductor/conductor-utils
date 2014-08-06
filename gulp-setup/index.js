916ee067cdc415a8096a01e3bf18280aeb3252c4
Christian Maher <maher.cs@gmail.com>
2014-08-05 21:30:56 -0400

    Take advantage of gulp-conductor-common

--- gulp-setup/index.js
+++ gulp-setup/index.js
@@ -2,6 +2,12 @@
 
 var fs = require('fs');
 var tasks = fs.readdirSync('./gulp-setup/tasks');
+var gulp = require('gulp');
+var conBuild = require('gulp-conductor-common');
+
+gulp.task('bower-json', conBuild('bower-json'));
+gulp.task('lint', conBuild('lint'));
+gulp.task('test', conBuild('test'));
 
 tasks.forEach(function(task) {
   require('./tasks/' + task);
--- gulp-setup/tasks/test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-
-gulp.task('test', ['lint'], function () {
-  return gulp.src(['./spec/spec-setup.js', './spec/**/*.spec.js'], { read: false })
-    .pipe(mocha({ reporter: 'spec' }));
-});
